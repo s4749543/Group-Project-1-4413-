1fa10ff8f79906e05ff7a83d2a4bb71981d1f174
s4749543 <s4749543@student.uq.edu.au>
2026-08-29 06:13:39 +0000

    update file

---
+++ analysis.r
@@ -0,0 +1,20 @@
+### 0. Packages and Setup ###
+
+packages.install <- c("dplyr", "readr","tidyr", "ggplot2", "lubridate", "broom")
+library(dplyr)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+library(broom)
+
+### 1. Import and Clean Data ###
+
+### 2. Task 1: Identify day 0 ###
+### 3. Task 2: Construct event windows ###
+### 4. Task 3: AR and CAR (-10,+10) ###
+### 5. Task 4: Announcement CAR and dilution ###
+### 6. Task 5: Utility vs industrial ###
+### 7. Task 6: Offer size regression ###
+### 8. Export tables and figures ###
+
